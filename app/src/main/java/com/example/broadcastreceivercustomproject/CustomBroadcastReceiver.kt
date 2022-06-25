f5ffdb16442a3b85a2c60988711e865807c24294
a1yak <alyak.grt@gmail.com>
2022-06-25 23:23:44 +0300

    sms app basic

--- app/src/main/java/com/example/broadcastreceivercustomproject/CustomBroadcastReceiver.kt
+++ app/src/main/java/com/example/broadcastreceivercustomproject/CustomBroadcastReceiver.kt
@@ -4,16 +4,43 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
+import android.os.Message
 import android.widget.Toast
+import android.telephony.SmsMessage
+import java.util.*
 
-class CustomBroadcastReceiver : BroadcastReceiver() {
+class CustomBroadcastReceiver(function: () -> Unit) : BroadcastReceiver() {
 
     lateinit var connectivityManager: ConnectivityManager
+    var counter = 0
 
     override fun onReceive(context: Context, intent: Intent) {
 
+       val broadcastIntent = Intent()
+        val bundle = intent.getBundleExtra("pdu")
+        var message = arrayOf<SmsMessage>()
+        var string = ""
+        if(bundle!=null) {
+            var pdu: ByteArray? = bundle.getByteArray("pdu")
+            while (counter< pdu?.size!!) {
+                message[counter] = SmsMessage.createFromPdu(pdu)
+                string += message[counter].displayOriginatingAddress
+                string += ": "
+                string += message[counter].messageBody
+                counter++
+            }
+        }
 
-        if(isOnline(context)){
+        Toast.makeText(context,string,Toast.LENGTH_SHORT).show()
+        broadcastIntent.putExtra("Message", string)
+        broadcastIntent.setAction("SMS_RECEIVED_ACTION")
+        context.sendBroadcast(broadcastIntent)
+    }
+}
+
+
+
+      /*  if(isOnline(context)){
             Toast.makeText(context, "Internet connected", Toast.LENGTH_SHORT).show()
         }
         else {
@@ -26,4 +53,4 @@ class CustomBroadcastReceiver : BroadcastReceiver() {
         val networkInfo = connectivityManager.activeNetworkInfo
         return (networkInfo!=null && networkInfo.isConnected)
     }
-}
+}*/
--- app/src/main/java/com/example/broadcastreceivercustomproject/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceivercustomproject/MainActivity.kt
@@ -1,22 +1,62 @@
 package com.example.broadcastreceivercustomproject
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
+import com.example.broadcastreceivercustomproject.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val intentFilter = IntentFilter()
+    private val SmsReceiver = CustomBroadcastReceiver(){
+        @Override
+        fun onReceive(){
+            binding.tvReceivedMsg.text=intent.extras?.getString("Messaga")
+        }
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val receiver = CustomBroadcastReceiver()
-        registerNetworkBroadcastReceiver(receiver);
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        intentFilter.addAction("SMS_RECEIVED_ACTION")
+        registerNetworkBroadcastReceiver(SmsReceiver)
+        binding.btnSend.setOnClickListener { sendMesage() }
+    }
+
+    fun sendMesage(){
+        val message: String = binding.tvMessage.text.toString()
+        val adressNumber: String =binding.tvAdress.text.toString()
+        var sentPI: PendingIntent = PendingIntent.getBroadcast(this,0, Intent("Message sent"),0)
+        var receivedPI: PendingIntent = PendingIntent.getBroadcast(this,0, Intent("Message delivered"),0)
+        val sms = SmsManager.getDefault()
+        sms.sendTextMessage(adressNumber, null, message, sentPI,receivedPI)
     }
 
     fun registerNetworkBroadcastReceiver(receiver: CustomBroadcastReceiver){
         if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
             registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+        }
+
+        @Override
+        fun onPause(){
+            super.onPause()
+            unregisterReceiver(SmsReceiver)
+        }
+
+        @Override
+        fun onResume(){
+            super.onResume()
+            registerReceiver(SmsReceiver, intentFilter)
         }
     }
 }
